df54800d490f308fb11cfa15f8d7d0d930754325
PandikalaPk <pkcse04@gmail.com>
2019-09-18 15:15:47 +0530

    1 file modified

--- index.js
+++ index.js
@@ -38,26 +38,51 @@ checkboxChange(event){
     return (
       <div>
         <Hello name={this.state.name} />
+      <div>
+      
         <h3 align ="left"> Search Options</h3> 
-         
+        <div>
+        
+        <h3 align ="left"> Hierarchy Comparison Direction</h3> 
+        <input type="radio" name="fruit" value="Compare Down Hierarchy"  onChange={this.checkboxChange}/> Compare Down Hierarchy
+        <input type="radio" name="fruit" value="Compare Across Hierarchy"  onChange={this.checkboxChange} />Compare Across Hierarchy
+        </div>
+        <div>
+        <h3 align ="left"> Excluded Status</h3> 
+        <h4>
+                <label>
+                  <input type="checkbox" value="Cash"/>
+                  Cash
+                </label>
+              </h4>
+          
+              <h4>
+                <label>
+                  <input type="checkbox" value="Delete"/>
+                  Delete
+                </label>
+              </h4>
+      </div>
+      </div>
+
         <h3 align ="left"> Region/Country</h3>  
         <div> 
              <h4>
                 <label>
-                  <input type="checkbox" value="All" onChange={this.checkboxChange}/>
+                  <input type="checkbox" value="All"/>
                   All
                 </label>
               </h4>
           
               <h4>
                 <label>
-                  <input type="checkbox" value="Misc" onChange={this.checkboxChange}/>
+                  <input type="checkbox" value="Misc"/>
                   Misc
                 </label>
               </h4>
             
         <input className="textBox" type="text" placeholder=" Quick Search" /> 
-        <h3>{this.state.checkboxValue}</h3>
+        <h3>User have Selected {this.state.checkboxValue}</h3>
         </div>
         <RegionCountry values={data}/>
       </div>
